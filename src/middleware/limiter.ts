ccef0a85b68b0631f221a4859d22d63248628419
Daniel88dev <64728456+Daniel88dev@users.noreply.github.com>
2026-07-25 19:31:57 +0200

    update limiter

--- src/middleware/limiter.ts
+++ src/middleware/limiter.ts
@@ -1,8 +1,8 @@
 import { rateLimit } from "express-rate-limit";
 
 export const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  limit: 100,
+  windowMs: 5 * 60 * 1000,
+  limit: 500,
   statusCode: 429,
   standardHeaders: true,
   legacyHeaders: false,
